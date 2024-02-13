55434a8959436cccffe687dc9d39f1334dfa72ef
Khush Bhadrecha <93341441+khushbhadrecha02@users.noreply.github.com>
2024-02-13 11:54:47 +0530

    areas

---
+++ Hackathon/Areas/ACCController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.Areas
+{
+    public class ACCController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
