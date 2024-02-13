409de84fe2fd96a8ff451805c28be1c44316da0d
Khush Bhadrecha <93341441+khushbhadrecha02@users.noreply.github.com>
2024-02-13 11:49:44 +0530

    New Controller ACC Created

---
+++ Hackathon/Areas/ACC/Controllers/ACCController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.Areas.ACC.Controllers
+{
+    public class ACCController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
