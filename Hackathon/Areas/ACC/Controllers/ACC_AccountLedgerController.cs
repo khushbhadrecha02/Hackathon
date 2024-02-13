4c99e05bdbf4afbb78b4d2b09cb5a98ffb23e683
Khush Bhadrecha <93341441+khushbhadrecha02@users.noreply.github.com>
2024-02-13 11:59:01 +0530

    areas/ctlller

---
+++ Hackathon/Areas/ACC/Controllers/ACC_AccountLedgerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hackathon.Areas.ACC.Controllers
+{
+    public class ACC_AccountLedgerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
